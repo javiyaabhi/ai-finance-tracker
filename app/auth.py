b9666cc69c9ffe1a4e20c68bd68b1e8118daca62
Abhi Javiya <javiyaabhi60@gmail.com>
2025-06-18 14:31:58 +0530

    Add user authentication system with register/login/logout

---
+++ app/auth.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from app.forms import RegistrationForm, LoginForm
+from app.models import User
+from app import db, bcrypt
+from flask_login import login_user, logout_user
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created!', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Check email and password.', 'danger')
+    return render_template('login.html', form=form)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
